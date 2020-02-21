8ce633a490eeec69ccdeb76bc5cc4065725d2971
jeevithac1999 <59138170+jeevithac1999@users.noreply.github.com>
2020-02-21 20:48:07 +0530

    teachers
    a page that displays teacher details with handlebars

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs=require("express-handlebars");
+const path=require("path");
 const teachersRouter = require("./routers/teachersRouter");
 const teacherRouter = require("./routers/teacherRouter");
+const teachers=require("./models/teachers");
+const formatIndex=require("./views/helpers/formatIndex");
 const app = express();
-app.get("/",(request,response)=>{
-  response.send("Teacher server");
+const hbs=expressHbs.create({
+  extname:".hbs",
+  layoutsDir: path.join(__dirname,"./views/layouts"),
+  partialsDir:path.join(__dirname,"./views/partials"),
+  helpers:{
+    formatIndex
+  }
 });
+app.engine(".hbs",hbs.engine);
+app.set("view engine",".hbs");
+app.set("views",path.join(__dirname,"./views"));
 app.use(bodyParser.json());
+app.get("/",(request,response)=>{
+  response.render("home",{
+    layout:"hero",
+    pageTitle:"Home",
+  });
+});
+app.get("/web/teachers",(request,response)=>{
+  response.render("teachers",{
+    layout:"navigation",
+    pageTitle:"Teachers",
+    teachers
+  })
+})
 app.use("/teachers", teachersRouter);
 app.use("/teacher", teacherRouter);
 const server=app.listen(8080, (request, response) => {
-  console.log(`Server running on port ${server.address.port}.`)
+  console.log(`Server running on port ${server.address().port}.`)
 });
